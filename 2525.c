073c1e3da3e1e62f8044d315fef34aa0392df895
이민규 <104059751+Mingyuuu0108@users.noreply.github.com>
2022-07-24 19:47:15 +0900

    Add files via upload

--- 2525.c
+++ 2525.c
@@ -1,14 +1,22 @@
 #include "stdio.h"
-int main(){
-    int h,m,t,Alltime=0;
-    scanf("%d %d\n%d",&h,&m,&t);
-    Alltime=h*60+m+t;
-    
-    if(Alltime/60<24){
-        printf("%d %d",Alltime/60,Alltime%60);
-    }
-    else if(Alltime/60>=24){
-        printf("%d %d",Alltime/60%24,Alltime%60);
-    }
-}
 
+int main(){
+    int n,a=0,b=0,plus = 50;
+    scanf("%d",&n);
+    
+    for(int i=0;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            if(i*j>=n && (i*j-j)<n){
+                
+                if(i+j < plus){
+                    plus = i+j;
+                    a = i;
+                    b = j;
+                }
+            }
+        }
+    }
+    printf("%d %d",a,b);
+    return 0;
+    
+}
